9c005ac17f85e18fe9dbba5077cbe5c3b56047e0
Vinay Joshi <vinay01joshi@gmail.com>
2017-08-21 08:40:55 +0530

    Angular Mateiral 2| Installing Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+
+import {MdCheckboxModule} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
