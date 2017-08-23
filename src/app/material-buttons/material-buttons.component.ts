fb1e31f23e83c86c6b79b272cda36f939782dc88
Vinay Joshi <vinay01joshi@gmail.com>
2017-08-23 08:27:03 +0530

    Angular Material 2 | Progress snipnner - Implementation - Part-Final

--- src/app/material-buttons/material-buttons.component.ts
+++ src/app/material-buttons/material-buttons.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/timer';
 
 @Component({
   selector: 'app-material-buttons',
   templateUrl: './material-buttons.component.html',
   styleUrls: ['./material-buttons.component.css']
 })
-export class MaterialButtonsComponent implements OnInit {
-
+export class MaterialButtonsComponent {
   catagories = [
     {name: 'Beginner'},
     {name: 'Intermediate'},
@@ -14,21 +15,28 @@ export class MaterialButtonsComponent implements OnInit {
   ];
   progress=0;
   timer;
+  isLoading= false;
   constructor() {
      this.timer = setInterval(()=> {
       this.progress++;
       if(this.progress ==100) clearInterval(this.timer)        
-    },20)
-   }
+    },20);
 
-  ngOnInit() {
+    this.isLoading = true;
+    this.getCoures()
+      .subscribe(x=> this.isLoading = false)
+
   }
-
+    
   selectCatagory(catagory){
     this.catagories
       .filter(c => c !=  catagory)
       .forEach( c=> c['selected'] = false);
     catagory.selected = !catagory.sele;
+  }
+
+  getCoures() {
+     return Observable.timer(2000);             
   }
 
 }
